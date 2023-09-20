f1a78307e52b947073c17434eae12b7720a012a2
Xomek <z_v_m_x@mail.ru>
2023-09-20 18:45:39 +0700

    create: TheHeader component and some styles

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header class="header">
+    <span class="logo">Жилфонд</span>
+    <span class="username">Пользователь</span>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import "../styles/palette.scss";
+
+.header {
+  padding: 30px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+
+.logo {
+  font-size: 32px;
+  color: $red;
+  font-weight: 700;
+}
+
+.username {
+  color: #333;
+  font-size: 16px;
+  font-weight: 400;
+}
+</style>
--- src/pages/TheHome.vue
+++ src/pages/TheHome.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheHeader from "../components/TheHeader.vue";
+</script>
 
 <template>
-  <div></div>
+  <div class="container">
+    <TheHeader />
+    <main class="main"></main>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.main {
+  display: grid;
+  grid-template-columns: 290px 1fr;
+  min-height: 575px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+}
+</style>
